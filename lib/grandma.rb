10018e24a9499ef580573d81c5eb845c2df9cac1
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2017-07-04 03:07:28 +0000

    finished speaking grandma lab ruby

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,19 @@
-# Write your code here!
+def speaktoGrandma(words)
+    if words == "BYE"
+        "Grandma left. WAAAAAAAAAAA"
+    elsif words == words.upcase
+        "NO, NOT SINCE #{1930 + rand(20)}!"
+    
+    else
+        "HUH?! SPEAK UP, SONNY!"
+    end
+end
+
+begin
+    puts "What do you want to say to Grandma?"
+    phrase = gets.chomp
+    puts speaktoGrandma(phrase)
+
+end while phrase != "BYE"
+
 
